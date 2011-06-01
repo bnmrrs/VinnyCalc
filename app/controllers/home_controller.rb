1e686de06ec57a44b6e348e2d65af9d32d4f385f
Ben Morris <ben@bnmrrs.com>
2011-06-01 08:40:07 -0400

    Added certs to foursquare client

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @logged_in = logged_in?
     if logged_in?
       logger.error 'here'
-      client = Foursquare2::Client.new(:oauth_token => session[:token])
+      client = Foursquare2::Client.new(:oauth_token => session[:token], :ssl => {:ca_path => "/etc/ssl/certs"})
       venue = client.venue(312090) # Vincenzo's venue id
       if venue[:beenHere]
         @visit_count = venue[:beenHere][:count]
