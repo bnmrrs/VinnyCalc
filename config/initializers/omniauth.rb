9089e67275fb70703514dd815b96c25932be8e86
Ben Morris <ben@bnmrrs.com>
2011-06-01 08:29:31 -0400

    Trying out certs path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :foursquare, "CHMMF54KFEVAHVDCGWUH1QQ3T5O1WCYNR3N3EQ22A2YHR4KZ", "PFC2T5GCSIACB3Z5OOK2APSLYVRDAMSS4D2DXYSR5ZR4D13N"
+  provider :foursquare, "CHMMF54KFEVAHVDCGWUH1QQ3T5O1WCYNR3N3EQ22A2YHR4KZ", "PFC2T5GCSIACB3Z5OOK2APSLYVRDAMSS4D2DXYSR5ZR4D13N", {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
